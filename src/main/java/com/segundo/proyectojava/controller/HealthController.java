1e939de7d16551df5d998d9516460777d7bb9171
Bryan Burgos <bry.burgos@duocuc.cl>
2026-03-17 15:36:23 -0300

    Agregue Service

--- src/main/java/com/segundo/proyectojava/controller/HealthController.java
+++ src/main/java/com/segundo/proyectojava/controller/HealthController.java
@@ -1,16 +1,26 @@
 package com.segundo.proyectojava.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.segundo.proyectojava.service.HealthService;
 
 
 
 @RestController
 @RequestMapping("/health")
+
+
 public class HealthController {
+
+    @Autowired
+    private HealthService healthService;
+
+
     @GetMapping
     public String healthCheck(){
-        return "La aplicacion esta funcionando correctamente";
+        return healthService.healthCheck();
     }
 
 
--- src/main/java/com/segundo/proyectojava/service/HealthService.java
+++ src/main/java/com/segundo/proyectojava/service/HealthService.java
@@ -1,5 +1,13 @@
 package com.segundo.proyectojava.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class HealthService {
+
+    public String healthCheck() {
+        return "Hola mundoooooooooooo";
+    }
+
 
 }
